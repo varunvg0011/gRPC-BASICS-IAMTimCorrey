05b0e3cc928e633b6983e80ea675bbea309b51ce
Varun <varunvg0011@gmail.com>
2023-08-30 22:41:45 +0530

    Learning basics of gRPC by creating server and client

---
+++ FirstGrpcProject/gRPCClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using gRPCClient;
+using GrpcServer;
+
+namespace gRPCClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Varun" };
+            var channel = GrpcChannel.ForAddress("https://localhost:7214");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(input);//the response we get
+
+            Console.WriteLine(reply.Message);
+            Console.ReadLine();
+
+        }
+    }
+}
